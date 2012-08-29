4f3d2d0fa0a53b0c9d23e83efc49031cd1dd04ae
Michael Hutchins <mlhutch@uw.edu>
2012-08-29 14:31:45 -0700

    Increased stability by checking and alterning packet length as needed (a reserved byte is occasionally dropped).

--- firstRunFiles/readTSIP.py
+++ firstRunFiles/readTSIP.py
@@ -26,21 +26,29 @@ while True:
 		start=hexline.find('108fac')
 		end=hexline.find('1003',start)
 		secTiming=hexline[start+6:end]
-		
-		fix=secTiming[12*2-2:12*2]
-		hexTemp=secTiming[32*2-2:35*2]
-		hexLat=secTiming[36*2-2:43*2]
-		hexLong=secTiming[44*2-2:51*2]
-		temp=struct.unpack('!f',hexTemp.decode('hex'))
-		lat=struct.unpack('!d',hexLat.decode('hex'))
-		long=struct.unpack('!d',hexLong.decode('hex'))
-		pi=3.1415926535898
-		temp=temp[0]
-		lat=lat[0] * 180 / pi
-		long = long[0] * 180 / pi
-		print 'Temperature: ' + str(temp) + \
-			', Latitude: ' + str(lat) + \
-			', Longitude: ' + str(long)
+		if (len(secTiming) > 130):
+			if (len(secTiming)==134):
+				secTiming=secTiming[:24*2] + '00' + secTiming[24*2:]	
+			fix=secTiming[12*2-2:12*2]
+			hexTemp=secTiming[32*2:35*2+2]
+			hexLat=secTiming[36*2:43*2+2]
+			hexLong=secTiming[44*2:51*2+2]
+#			print 'Temp: ' + hexTemp
+#                        print 'Lat: ' + hexLat
+#                        print 'Long: ' + hexLong
+			temp=struct.unpack('!f',hexTemp.decode('hex'))
+			lat=struct.unpack('!d',hexLat.decode('hex'))
+			long=struct.unpack('!d',hexLong.decode('hex'))
+			pi=3.1415926535898
+			temp=temp[0]
+			lat=lat[0] * 180 / pi
+			long = long[0] * 180 / pi
+			print 'Temperature: ' + str(temp) + \
+				', Latitude: ' + str(lat) + \
+				', Longitude: ' + str(long)
+		else:
+			time.sleep(0.17)
+			print 'Packet Length: ' + str(len(secTiming))
 #	print ser.read(ser.inWaiting()).encode('hex')
 #	print binhex.binascii.hexlify(ser.readline(1000))
 #	print ' '
