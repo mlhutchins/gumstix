4d177dcf82e333d702603fd66f73009569db40c9
Michael Hutchins <mlhutch@uw.edu>
2012-08-29 14:10:04 -0700

    readTSIP can semi-reliable report temperature, latitude and longitude.

--- firstRunFiles/readTSIP.py
+++ firstRunFiles/readTSIP.py
@@ -1,6 +1,7 @@
 import serial
 import time
 import binhex
+import struct
 # Configure serial connection
 ser = serial.Serial(
 	port='/dev/ttyO0',
@@ -18,7 +19,28 @@ while True:
 	line=ser.read(ser.inWaiting())
 	if (len(line) > 0 ):
 		print line.encode('hex')
-
+		hexline=line.encode('hex')
+		start=hexline.find('108fab')
+		end=hexline.find('1003')
+		primTiming=hexline[start+6:end]
+		start=hexline.find('108fac')
+		end=hexline.find('1003',start)
+		secTiming=hexline[start+6:end]
+		
+		fix=secTiming[12*2-2:12*2]
+		hexTemp=secTiming[32*2-2:35*2]
+		hexLat=secTiming[36*2-2:43*2]
+		hexLong=secTiming[44*2-2:51*2]
+		temp=struct.unpack('!f',hexTemp.decode('hex'))
+		lat=struct.unpack('!d',hexLat.decode('hex'))
+		long=struct.unpack('!d',hexLong.decode('hex'))
+		pi=3.1415926535898
+		temp=temp[0]
+		lat=lat[0] * 180 / pi
+		long = long[0] * 180 / pi
+		print 'Temperature: ' + str(temp) + \
+			', Latitude: ' + str(lat) + \
+			', Longitude: ' + str(long)
 #	print ser.read(ser.inWaiting()).encode('hex')
 #	print binhex.binascii.hexlify(ser.readline(1000))
 #	print ' '
