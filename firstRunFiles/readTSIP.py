abed7da25fda63b57e3fba701122992fc56255ba
Michael Hutchins <mlhutch@uw.edu>
2012-08-29 14:45:18 -0700

    Added comments.

--- firstRunFiles/readTSIP.py
+++ firstRunFiles/readTSIP.py
@@ -1,7 +1,11 @@
+# Python program to read Resolution T serial TSIP messages
+
+# Import modules
 import serial
 import time
 import binhex
 import struct
+
 # Configure serial connection
 ser = serial.Serial(
 	port='/dev/ttyO0',
@@ -11,14 +15,16 @@ ser = serial.Serial(
 	bytesize=serial.EIGHTBITS
 )
 
-#ser.open()
-#ser.isOpen()
-
+# Continuously monitor serial line
 while True:
+	
+	# Wait 0.25 seconds and check messages in serial buffer
 	time.sleep(0.25)
 	line=ser.read(ser.inWaiting())
 	if (len(line) > 0 ):
 		print line.encode('hex')
+
+		# Extract primary and secondary timing messages
 		hexline=line.encode('hex')
 		start=hexline.find('108fab')
 		end=hexline.find('1003')
@@ -26,29 +32,36 @@ while True:
 		start=hexline.find('108fac')
 		end=hexline.find('1003',start)
 		secTiming=hexline[start+6:end]
+		
+		# Check for proper length of secondary timing message
 		if (len(secTiming) > 130):
+			
+			# Pad message if reserved byte gets dropped
 			if (len(secTiming)==134):
 				secTiming=secTiming[:24*2] + '00' + secTiming[24*2:]	
+
+			# Extract parameters of interest
 			fix=secTiming[12*2-2:12*2]
 			hexTemp=secTiming[32*2:35*2+2]
 			hexLat=secTiming[36*2:43*2+2]
 			hexLong=secTiming[44*2:51*2+2]
-#			print 'Temp: ' + hexTemp
-#                        print 'Lat: ' + hexLat
-#                        print 'Long: ' + hexLong
+
+			# Convert from hex to floating point decimal
 			temp=struct.unpack('!f',hexTemp.decode('hex'))
 			lat=struct.unpack('!d',hexLat.decode('hex'))
 			long=struct.unpack('!d',hexLong.decode('hex'))
+			
+			# Convert from radians to degrees, pi is value hardcoded by Trimble
 			pi=3.1415926535898
 			temp=temp[0]
 			lat=lat[0] * 180 / pi
 			long = long[0] * 180 / pi
+
+			# Print results
 			print 'Temperature: ' + str(temp) + \
 				', Latitude: ' + str(lat) + \
 				', Longitude: ' + str(long)
 		else:
+			# Offset time if messages are being read partway through
 			time.sleep(0.17)
 			print 'Packet Length: ' + str(len(secTiming))
-#	print ser.read(ser.inWaiting()).encode('hex')
-#	print binhex.binascii.hexlify(ser.readline(1000))
-#	print ' '
