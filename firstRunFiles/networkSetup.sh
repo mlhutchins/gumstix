1dbfd1c882c9eb5a894d518d0bcd95ddae594150
Michael Hutchins <mlhutch@uw.edu>
2013-01-04 11:31:36 -0800

    Added in a small countdown to the networkSetup script.

--- firstRunFiles/networkSetup.sh
+++ firstRunFiles/networkSetup.sh
@@ -2,7 +2,18 @@ IP=128.208.22.30
 GATEWAY=128.208.22.100
 NETMASK=255.255.255.0 
 
-sleep 60
+echo "Switch to main ethernet in 60 seconds"
+
+sleep 30
+
+echo "30 seconds"
+
+sleep 20
+
+echo "10 seconds"
+
+sleep 10
+
 ifconfig eth0 down
 ifconfig eth0 $IP netmask $NETMASK up
 route add default gw $GATEWAY
