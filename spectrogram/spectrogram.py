39c8b14b42f86a9567893b227987e4ac2b2619ec
Michael Hutchins <mlhutch@uw.edu>
2013-02-20 17:11:21 -0800

    The fft and power spectra array part are done.

--- spectrogram/spectrogram.py
+++ spectrogram/spectrogram.py
@@ -31,7 +31,7 @@ Ny = len(y) # Sample length
 # Create Hanning window
 j = numpy.arange(1.0,Nw+1)
 w = 0.5 * (1 - numpy.cos(2*numpy.pi*(j-1)/Nw))
-varw = 3/8
+varw = 3./8.
 
 # Window the data
 nwinf = numpy.floor(Ny/Nw)
@@ -45,5 +45,17 @@ yw[:,1:(nwin-1):2] = y[(Nw/2):(nwinf-0.5)*Nw].reshape(Nw,nwinh,order='F').copy()
 
 # Taper the data
 yt = yw * numpy.tile(w,(nwin,1)).T.shape
+
+# DFT of the data
+ythat = numpy.zeros(yt.shape)
+ythat = ythat + 0j
+for i in range(yt.shape[1]):
+	ythat[:,i] = scipy.fft(yt[:,i])
+S = (numpy.absolute(ythat)**2)/varw
+S = S[0:Nw/2,:]
+SdB = 10*numpy.log10(S)
+Mw = numpy.arange(0,Nw/2)
+fw = Fs * Mw / Nw
+tw = numpy.arange(1,nwin) * 0.5 * Nw/Fs
 
 
