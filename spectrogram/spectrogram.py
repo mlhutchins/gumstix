93e6d8f089a7ed705af1c7f1c065895b5bc2cc89
Michael Hutchins <mlhutch@uw.edu>
2013-02-20 16:00:52 -0800

    Can read in WB parameters and y data, need to remove bitstream dependency and do it with numpy.

--- spectrogram/spectrogram.py
+++ spectrogram/spectrogram.py
@@ -4,21 +4,21 @@ from bitstring import BitArray, BitStream
 
 ## Read in Wideband VLF Data
 
-# Opens data file as binary file
-fd = open('../../MATLAB/WBTest.dat', 'rb')
-f = BitStream(fd)
-# Import data as array
-#	fd is filename
-#	dtype is data type, currently correct version is unknown
-#	count is how many lines, -1 is EOF
-#	sep is delimiter, "" is for binary
+fileName = '../../../MATLAB/WBTest.dat'
 
+# Opens data file as binary file
+fd = open(fileName, 'rb')
+f = BitStream(fd)
+
+# Read WB data parameters
 time = f.read(32).intle
 Fs = f.read(64).floatle
 offset = f.read(64).floatle
 
-#time = numpy.fromfile(fd,dtype=numpy.float64,count=1,sep="")
-#Fs = numpy.fromfile(fd,dtype=float,count=2,sep="")
-#offset = numpy.fromfile(fd,dtype=float,count=3,sep="")
-#data = numpy.fromfile(fd,dtype=numpy.float32,count=-1,sep="")
+fd = open(fileName, 'rb')
+
+y = numpy.fromfile(fd, dtype=numpy.dtype('<i2'))
+y = y[10:]
+print len(y)
+print y[:10]
 
