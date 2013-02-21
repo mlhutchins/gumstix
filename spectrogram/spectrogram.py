4f9d6dca8b725c6b6c9eba8ea9952deb585b707d
Michael Hutchins <mlhutch@uw.edu>
2013-02-20 16:23:18 -0800

    Created the time base for the data based on the sampling rate and initial time offset.

--- spectrogram/spectrogram.py
+++ spectrogram/spectrogram.py
@@ -1,5 +1,6 @@
 import scipy
 import numpy
+import array
 
 ## Read in Wideband VLF Data
 
@@ -11,6 +12,15 @@ time = numpy.fromfile(fid, dtype=numpy.dtype('<i4'), count = 1)
 Fs = numpy.fromfile(fid, dtype=numpy.dtype('<f8'), count = 1)
 offset = numpy.fromfile(fid, dtype=numpy.dtype('<f8'), count = 1)
 y = numpy.fromfile(fid, dtype=numpy.dtype('<i2'))
-y = y[10:]
-print len(y)
-print y[:10]
+
+## Normalize to soundcard units and switch to float
+y = y.astype(numpy.float)
+y = y/32768
+
+## Make the time base
+
+t = numpy.arange(0.0,len(y))
+t = t + offset
+t = t/Fs
+
+
