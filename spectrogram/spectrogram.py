49734f533237d6de6817d3c4423ca2e957b39219
Michael Hutchins <mlhutch@uw.edu>
2013-02-20 16:52:09 -0800

    Spectrogram through applying the taper works. Now to start on FFT section (and comment eventually).

--- spectrogram/spectrogram.py
+++ spectrogram/spectrogram.py
@@ -23,4 +23,27 @@ t = numpy.arange(0.0,len(y))
 t = t + offset
 t = t/Fs
 
+## Spectrogram ##
+
+Nw = 2**10 # Hanning window length
+Ny = len(y) # Sample length
+
+# Create Hanning window
+j = numpy.arange(1.0,Nw+1)
+w = 0.5 * (1 - numpy.cos(2*numpy.pi*(j-1)/Nw))
+varw = 3/8
+
+# Window the data
+nwinf = numpy.floor(Ny/Nw)
+nwinh = nwinf - 1
+nwin = nwinf + nwinh
+
+# Fill in the windows array
+yw = numpy.zeros((Nw,nwin))
+yw[:,0:nwin:2] = y[:nwinf*Nw].reshape(Nw,nwinf,order='F').copy()
+yw[:,1:(nwin-1):2] = y[(Nw/2):(nwinf-0.5)*Nw].reshape(Nw,nwinh,order='F').copy()
+
+# Taper the data
+yt = yw * numpy.tile(w,(nwin,1)).T.shape
+
 
