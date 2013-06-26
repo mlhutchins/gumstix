6e8f4318e918f252bedb2b8a4a8848c4e7301374
Michael Hutchins <mlhutch@uw.edu>
2013-06-26 12:52:42 -0700

    Refactored readTSIP to make the read lines functions.

--- firstRunFiles/readTSIP.py
+++ firstRunFiles/readTSIP.py
@@ -96,6 +96,103 @@ def extractMessage(idpacket,hexline):
 		packet = str(-1)
 	return packet
 
+			
+def printPrimary(primTiming):
+
+	# Select and convert date (Hex string to decimal to string)
+	seconds=str(int(primTiming[10*2-2:10*2],16))
+	minutes=str(int(primTiming[11*2-2:11*2],16))
+	hours=str(int(primTiming[12*2-2:12*2],16))
+	day=str(int(primTiming[13*2-2:13*2],16))
+	month=str(int(primTiming[14*2-2:14*2],16))
+	year=str(int(primTiming[15*2-2:],16))
+
+	# Pad digits
+	if (len(seconds)==1):
+		seconds='0'+seconds
+	if (len(minutes)==1):
+		minutes='0'+minutes
+	if (len(hours)==1):
+		hours='0'+hours
+	if (len(day)==1):
+		day='0'+day
+	if (len(month)==1):
+		month='0'+month	
+	
+	# Extract timing flag
+	timing=bin(int(primTiming[9*2-2:9*2],16))[2:]
+
+	# Pad timing flag
+	if (len(timing)<8):
+		padding='0'*(8-len(timing))	
+		timing=padding+timing
+
+	# Get timing info from flags
+	timingInfo=''
+	for i in range(0,5):
+		timingInfo = timingInfo + ', '+ timingFlags[str(i)+str(timing[7-i])]
+			
+	# Print results
+	statement1 = year+'/'+month+'/'+day+' '+hours+':'+minutes+':'+seconds # + ', ' + timingInfo[2:]
+	return statement1
+
+
+def printSecondary(secTiming)
+	
+			# Extract parameters of interest
+	fix=secTiming[12*2-2:12*2]
+	hexTemp=secTiming[32*2-2:35*2]
+	hexLat=secTiming[36*2-2:43*2]
+	hexLong=secTiming[44*2-2:51*2]
+	alarms=bin(int(secTiming[10*2-2:11*2],16))[2:]		
+
+	# Pad alert bytes
+	if (len(alarms)<13):
+		padding='0'*(12-len(alarms))
+		alarms=padding+alarms+'0'
+	
+	# Select current alerts
+	alerts[:]=[];
+	for i in range(0,13):
+		if ((alarms[i]=='1') and (alertList[i] not in 'Not used')):
+			alerts.append(alertList[i])
+
+	# Convert from hex to floating point decimal
+	temp=struct.unpack('!f',hexTemp.decode('hex'))
+	lat=struct.unpack('!d',hexLat.decode('hex'))
+	long=struct.unpack('!d',hexLong.decode('hex'))
+	
+	# Convert from radians to degrees, pi is value hardcoded by Trimble
+	pi=3.1415926535898
+	temp=temp[0]
+	lat=lat[0] * 180 / pi
+	long = long[0] * 180 / pi
+
+	# Print results
+	statement2a = 'Latitude: ' + str(lat) + \
+		', Longitude: ' + str(long) + \
+		', Temperature: ' + str(temp)
+		
+	# Print GPS Status
+	statement2b = 'GPS Status: ' + gpsStatus[fix] # + ' (' + fix + ')'
+
+	# Print alerts, if any
+	if (len(alerts)>0):
+		statement2c = 'Current Alerts: ' + str(alerts)[1:-1]
+	else:
+		statement2c = ''
+
+	return (statement2a, statement2b, statement2v)
+
+def printTrack(trackline):
+	if (len(trackline)>2):
+		track = str(int(trackline[0],16))
+	else:
+		track = '-1'
+
+	statement3 = 'Satellites tracked: ' + track
+
+
 # Initialize variables
 removeIndex=[]
 timeIndex=0
@@ -110,11 +207,11 @@ while True:
 
 	currentTime = time.localtime()
 	
-	if (timeIndex==0.5):
-		ser.write('10271003'.decode('hex'))
-		ser.write('10241003'.decode('hex'))
-		line=[]
-	elif (timeIndex==1.0):
+#	if (timeIndex==0.5):
+#		ser.write('10271003'.decode('hex'))
+#		ser.write('10241003'.decode('hex'))
+#		line=[]
+	if (timeIndex==1.0):
 		line=ser.read(ser.inWaiting())
 		timeIndex=0
 	else:
@@ -124,7 +221,7 @@ while True:
 #		print 'Main hexline: ' + line.encode('hex')
 
 		# Read serial buffer
-                hexline=line.encode('hex')
+		hexline=line.encode('hex')
 	
 		# Extract TSIP messagess
 		primTiming = extractMessage('108fab',hexline)
@@ -137,93 +234,14 @@ while True:
 
 		# Check for proper length of primary timing message
 		if (len(primTiming) == 32):
-		
-			# Select and convert date (Hex string to decimal to string)
-			seconds=str(int(primTiming[10*2-2:10*2],16))
-			minutes=str(int(primTiming[11*2-2:11*2],16))
-			hours=str(int(primTiming[12*2-2:12*2],16))
-			day=str(int(primTiming[13*2-2:13*2],16))
-			month=str(int(primTiming[14*2-2:14*2],16))
-			year=str(int(primTiming[15*2-2:],16))
-
-			# Pad digits
-			if (len(seconds)==1):
-				seconds='0'+seconds
-			if (len(minutes)==1):
-				minutes='0'+minutes
-			if (len(hours)==1):
-				hours='0'+hours
-			if (len(day)==1):
-				day='0'+day
-			if (len(month)==1):
-				month='0'+month	
-	
-			# Extract timing flag
-			timing=bin(int(primTiming[9*2-2:9*2],16))[2:]
-
-			# Pad timing flag
-			if (len(timing)<8):
-				padding='0'*(8-len(timing))
-				timing=padding+timing
-
-			# Get timing info from flags
-			timingInfo=''
-			for i in range(0,5):
-				timingInfo = timingInfo + ', '+ timingFlags[str(i)+str(timing[7-i])]
-			
-			# Print results
-			
-			statement1 = year+'/'+month+'/'+day+' '+hours+':'+minutes+':'+seconds # + ', ' + timingInfo[2:]
-
+			statement1 = printPrimary(primTiming)		
 		else:
 			time.sleep(0.07)
 			statement1 = 'Primary Packet Length: ' + str(len(primTiming))
 		
 		# Check for proper length of secondary timing message
 		if (len(secTiming) == 134):
-			
-			# Extract parameters of interest
-			fix=secTiming[12*2-2:12*2]
-			hexTemp=secTiming[32*2-2:35*2]
-			hexLat=secTiming[36*2-2:43*2]
-			hexLong=secTiming[44*2-2:51*2]
-			alarms=bin(int(secTiming[10*2-2:11*2],16))[2:]		
-	
-			# Pad alert bytes
-			if (len(alarms)<13):
-				padding='0'*(12-len(alarms))
-				alarms=padding+alarms+'0'
-			
-			# Select current alerts
-			alerts[:]=[];
-			for i in range(0,13):
-				if ((alarms[i]=='1') and (alertList[i] not in 'Not used')):
-					alerts.append(alertList[i])
-
-			# Convert from hex to floating point decimal
-			temp=struct.unpack('!f',hexTemp.decode('hex'))
-			lat=struct.unpack('!d',hexLat.decode('hex'))
-			long=struct.unpack('!d',hexLong.decode('hex'))
-			
-			# Convert from radians to degrees, pi is value hardcoded by Trimble
-			pi=3.1415926535898
-			temp=temp[0]
-			lat=lat[0] * 180 / pi
-			long = long[0] * 180 / pi
-
-			# Print results
-			statement2a = 'Latitude: ' + str(lat) + \
-				', Longitude: ' + str(long) + \
-				', Temperature: ' + str(temp)
-				
-			# Print GPS Status
-			statement2b = 'GPS Status: ' + gpsStatus[fix] # + ' (' + fix + ')'
-
-			# Print alerts, if any
-			if (len(alerts)>0):
-				statement2c = 'Current Alerts: ' + str(alerts)[1:-1]
-			else:
-				statement2c=''
+			(statement2a, statement2b, statement2c) = printSecondary(secTiming)
 		else:
 			# Offset time if messages are being read partway through
 			time.sleep(0.07)
@@ -233,12 +251,7 @@ while True:
 
 		# Report the number of satellites locked and available
 
-		if (len(trackline)>2):
-			track = str(int(trackline[0],16))
-		else:
-			track = '-1'
-
-		statement3 = 'Satellites tracked: ' + track
+		statement3 = printTrack(tackline)
 
 		# Print out generated messages
 
