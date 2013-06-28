e72a1c53f3b95bbd1e2e26dc46bc2e9883f04e61
Michael Hutchins <mlhutch@uw.edu>
2013-06-28 15:17:05 -0700

    Put all of the NMEA settings into the same line. Switched to GGA messages only.

--- serial/sendTSIP.py
+++ serial/sendTSIP.py
@@ -16,8 +16,8 @@ messages={'01':['101e4b1003'.decode('hex')], # Hard Reset
         '03':['10bb00070204ff3db2b8c2408000004140000040c00000dd01ffffffffffffffffffffffffffffffffff1003'.decode('hex'), # General settings: O-D clock mode, stationary, 5 deg elev mask, dpgs auto off, datum wgs-84, anti-jamming
              '108e4e041003'.decode('hex'), # Set PPS to need >= 3 satellites
 			 '108ea5004500001003'.decode('hex')], # Set auto event packet mask (enable number of satellites)
-		'06':['107a0001000000781003'.decode('hex')], #Set NMEA
-		'07':['10bc000606030000000204001003'.decode('hex')], #Switch to NMEA	
+		'07':['107a0001000000011003'.decode('hex'), #Set NMEA settings
+			  '10bc000606030000000204001003'.decode('hex')], #Switch to NMEA messages (4800 8N1)
 		'50':['108e261003'.decode('hex')], # Write to ROM
 		'99':'Exit'
 }
