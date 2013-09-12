b7b9b8530afb416be9df6552eb96b1581de0d55d
Michael Hutchins <mlhutch@uw.edu>
2013-09-12 15:11:59 -0700

    Added the correct .profile and removed startGPSD.py in the upgrade script for 1.6.

--- upgrade_scripts/1-5to1-6/upgrade.sh
+++ upgrade_scripts/1-5to1-6/upgrade.sh
@@ -5,6 +5,13 @@ cp version.txt /home/host/
 echo 'Updated TSIP routines'
 cp tsip.py /home/sferix/gps/
 cp tsip.py /home/host/gps/
+rm /home/sferix/gps/startGPSD.py
+rm /home/host/gps/startGPSD.py
+
+echo 'Updating paths'
+cp profile /home/sferix/.profile
+cp profile /home/host/.profile
+cp profile /home/root/.profile
 
 echo 'Software 1.5 to 1.6 upgrade finished'
 echo ' '
