8b62204c666456e99aa80f9ed2055a7f60fa4aa3
Michael Hutchins <mlhutch@uw.edu>
2013-09-13 12:52:13 -0700

    Added UTC timing to GPS configuration.

--- serial/sendTSIP.py
+++ serial/sendTSIP.py
@@ -17,6 +17,7 @@ messages={'01':['101e4b1003'.decode('hex')], # Hard Reset
 		'02':['101e0e1003'.decode('hex')], # Warm Reset
         '03':['10bb00070204ff3db2b8c2408000004140000040c00000dd01ffffffffffffffffffffffffffffffffff1003'.decode('hex'), # General settings: O-D clock mode, stationary, 5 deg elev mask, dpgs auto off, datum wgs-84, anti-jamming
              '108e4e041003'.decode('hex'), # Set PPS to need >= 3 satellites
+			 '108ea2ff1003'.decode('hex'), # Set time reporting to UTC
 			 '108ea5004500001003'.decode('hex')], # Set auto event packet mask (enable number of satellites)
 		'07':['107a0001000000011003'.decode('hex'), #Set NMEA settings
 			  '10bc000606030000000204001003'.decode('hex')], #Switch to NMEA messages (4800 8N1)
@@ -44,7 +45,7 @@ while True:
 		break
 	elif result in 'raw':
 		raw=raw_input('Message:')
-		messages = {'raw':[raw]}
+		messages = {'raw':[raw.decode('hex')]}
 	elif result in '07':
 		print '	Switching to NMEA can only be reversed by a manual power cycle.'
 		confirm = raw_input('	Continue (y/n)?')
